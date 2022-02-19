24bd1f101f5dfa849567353266d86cb69fa6a288
Vladiusha <kalachikov.moyo@gmail.com>
2022-02-19 02:42:35 +0100

    i

--- App.js
+++ App.js
@@ -212,7 +212,7 @@ function setCSsByDevice(width) {
 }
 
 function menuButton() {
-    alert('LLLLL');
+    alert('HILLOU');
     let text_wrapper = document.getElementById('player_wrapper');
     let site_footer = document.getElementById('site_footer');
     let play = document.getElementById('play');
